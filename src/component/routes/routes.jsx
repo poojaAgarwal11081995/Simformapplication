f0e4507b52fbeb074f9fce3484814e33995736c5
DESKTOP-237LHVF\kapil <pooja.agarwal6994@gmail.com>
2021-05-18 17:34:23 +0530

    react-delete-offline lib. import

--- src/component/routes/routes.jsx
+++ src/component/routes/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch ,Redirect} from 'react-router-dom';
+import { Offline, Online } from "react-detect-offline";
 import {useCookies } from 'react-cookie';
 import Login from '../localauth/login';
 import Profile from '../localauth/profile';
@@ -7,11 +8,20 @@ import {PROFILE_PATH, LOGIN_PATH} from "../../path";
 import {USER_DETAILS} from "../../constant";
 function routes() {
     return (
+      <div>
+<Online>
         <Switch>
-           <Route path="/"  exact render={() => <Redirect to="/profile" />}/>
+        <Route path="/"  exact render={() => <Redirect to="/profile" />}/>
         <Route exact path={LOGIN_PATH} component={Login} />
         <PrivateRoutes exact path={PROFILE_PATH} component={Profile} />   
       </Switch>
+      </Online>
+      <Offline>
+      offline (surprise!)
+      </Offline>
+      </div>
+      
+       
     )
 }
 const PrivateRoutes = ({ component:Component, ...rest }) => {
